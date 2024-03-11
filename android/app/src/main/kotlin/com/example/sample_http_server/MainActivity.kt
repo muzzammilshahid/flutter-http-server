97e1c13829545c03c2fb7195937f6d924315b8c8
Muzzammil Shahid <muzzammilshahid50@gmail.com>
2024-03-11 13:10:54 +0500

    fix build

--- android/app/src/main/kotlin/com/example/sample_http_server/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/sample_http_server/MainActivity.kt
@@ -1,40 +1,5 @@
-//package com.example.sample_http_server
-//import io.flutter.embedding.android.FlutterActivity
-//
-//
-//class MainActivity: FlutterActivity()
-
-
-package com.example.sample_http_server;
-
-import android.content.Intent
-import androidx.annotation.NonNull
+package com.example.sample_http_server
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity : FlutterActivity() {
-    private val CHANNEL = "example_service"
 
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
-            // Note: this method is invoked on the main thread.
-                call, result ->
-            when (call.method) {
-                "startExampleService" -> {
-                    startService(Intent(this, ExampleService::class.java))
-                    result.success("Started!")
-                }
-                "stopExampleService" -> {
-                    stopService(Intent(this, ExampleService::class.java))
-                    result.success("Stopped!")
-                }
-                else -> {
-                    result.notImplemented()
-                }
-            }
-        }
-    }
-}
+class MainActivity: FlutterActivity()
